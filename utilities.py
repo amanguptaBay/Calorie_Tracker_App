3605f2c4c7dcf9dd2306cef089ed85edfcffa8a5
Aman <47506369+amanguptaBay@users.noreply.github.com>
2023-10-06 21:01:17 -0700

    More generic decorator function to clean up logic

--- utilities.py
+++ utilities.py
@@ -13,11 +13,17 @@ def Debug_User_Input(debug_user_input):
     """
         Adds a proprety to the function that is mock user input, used for testing
     """
+    return Bind_Arbitrary_Property("debug_user_input", debug_user_input)
+
+def Bind_Arbitrary_Property(pName, pValue):
+    """
+        Adds a proprety to the function that is mock user input, used for testing
+    """
     def outer(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             return func(*args, **kwargs)
-        wrapper.debug_user_input = debug_user_input
+        setattr(wrapper, pName, pValue)
         return wrapper
     
     return outer
